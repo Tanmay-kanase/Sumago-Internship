9c04b414bae26140732dd7ee6102a742796dde07
Tanmay Kanase <tanmaykanase06@gmial.com>
2025-01-10 23:19:56 +0530

    Basic html

--- html/Demo/src/App.jsx
+++ html/Demo/src/App.jsx
@@ -1,56 +1,10 @@
+import "./style.css";
+
 function App() {
   return (
     <>
-      <h1>Welcome to sumago</h1>
-      <h2>Welcome to sumago</h2>
-      <h3>Welcome to sumago</h3>
-      <h4>Welcome to sumago</h4>
-      <h5>Welcome to sumago</h5>
-      {/* <!-- <img src="Screenshot (17).png" alt="pic" />  --> */}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        atque ipsa labore autem aliquam nesciunt numquam! Exercitationem
-        pariatur labore iure fuga optio non dicta rerum, sit perferendis eum
-        debitis laudantium?
-      </p>
-      <table>
-        <tbody>
-          <th>
-            <td>Sr No</td>
-            <td>First Name </td>
-            <td>Last Name</td>
-          </th>
-          <tr>
-            <td>1</td>
-            <td>Tanmay</td>
-            <td>Kanase</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Sanket</td>
-            <td>Pardhi</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d7440.783469114714!2d73.87616726365543!3d18.535986716217987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smpas!5e0!3m2!1sen!2sin!4v1736400493133!5m2!1sen!2sin"
-        width="600"
-        height="450"
-        style="border:0;"
-        allowfullscreen=""
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/mz0awfKKTVY?si=SMQulAOemDCmDDRP&amp;start=4"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+      <h1 s>Hello Sumago</h1>
+      <h2>Hello Java Batch</h2>
     </>
   );
 }
