47600864f4880839310b0eee8d625240ceb29ae2
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-01-06 10:46:50 +0530

    .

--- firstTask/client/src/App.jsx
+++ firstTask/client/src/App.jsx
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Chat />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
